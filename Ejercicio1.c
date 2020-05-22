4d30444751c5a29ac89b23b722425cc540028e97
Narbis Tamiche <54629185+NarbisCOD@users.noreply.github.com>
2020-05-22 06:54:14 -0300

    Add files via upload

---
+++ Ejercicio1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    const int ANIOS_PORHORA = 7;
+    int horas_planeta = 1;
+
+
+    printf("Escribir la cantidad de horas que paso en el Planeta:\n");
+    scanf("%i", &horas_planeta);
+    printf("Total de anios Transcurridos: %i", horas_planeta*ANIOS_PORHORA);
+
+
+    return 0;
+}
